42a300ca394d4d50bb184ad640a4424480a45c3f
JMBlankley <jmblankley@outlook.com>
2024-02-15 17:40:26 -0600

    got phone numbers to show up on network page

--- pages/Network.js
+++ pages/Network.js
@@ -4,6 +4,16 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import AddButton from '../components/AddButton';
 import Contact from '../components/Contact';
 
+// Function to format phone number as (###) ###-####
+const formatPhoneNumber = (phoneNumber) => {
+  const cleaned = ('' + phoneNumber).replace(/\D/g, '');
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+  }
+  return null;
+};
+
 const Network = () => {
   const [contacts, setContacts] = useState([]);
 
@@ -12,10 +22,20 @@ const Network = () => {
       const { status } = await Contacts.requestPermissionsAsync();
       if (status === 'granted') {
         const { data } = await Contacts.getContactsAsync({
-          fields: [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers],
+          fields: [Contacts.Fields.FirstName, Contacts.Fields.PhoneNumbers]
         });
 
-        setContacts(data);
+        // Remove country code from phone numbers and format them
+const formattedContacts = data.map(contact => ({
+  ...contact,
+  phoneNumbers: contact.phoneNumbers ? contact.phoneNumbers.map(phone => ({
+    ...phone,
+    number: phone.number.replace(/^\+?1?/, '') // Remove +1 country code and leading 1
+  })) : []
+}));
+
+
+        setContacts(formattedContacts);
       }
     })();
   }, []);
@@ -26,7 +46,11 @@ const Network = () => {
         <View style={styles.container}>
           <Text style={styles.title}>Contacts Module Example</Text>
           {contacts.map((contact, index) => (
-            <Contact key={index} name={contact.name} />
+            <Contact
+              key={index}
+              name={contact.name}
+              phone={contact.phoneNumbers && contact.phoneNumbers.length > 0 ? formatPhoneNumber(contact.phoneNumbers[0].number) : 'No Phone Number'}
+            />
           ))}
         </View>
       </ScrollView>
@@ -40,8 +64,8 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     paddingHorizontal: 40,
-	  paddingBottom: 40,
-	  paddingTop: 10,
+    paddingBottom: 40,
+    paddingTop: 10,
     backgroundColor: "white"
   },
   title: {
